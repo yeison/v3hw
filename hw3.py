bf3de2005ce9018309d124a1858efa5c12d2e662
Yeison Rodriguez <yeirodr@gmail.com>
2010-05-01 23:16:16 -0400

    Plotting directly with R functions.

--- hw3.py
+++ hw3.py
@@ -13,7 +13,7 @@ scale = 3
 redPixels = []
 bluePixels = []
 histoRed = []
-histoBlue = []
+histoBlue = [0 for i in range(256)]
 histoRedI = []
 histoBlueI = []
 width = range(scale, (bird.width-1) - scale)
@@ -22,7 +22,7 @@ for i in width:
     for j in height:
         contrast = None
         for theta in range(1):
-            contrast = max(contrast, abs(int(dofIA2(i, j, (theta*pi)/4, scale, bird_BW))))
+            contrast = max(contrast, abs(dofIA2(i, j, (theta*pi)/4, scale, bird_BW)))
 
         if bird[j][i][2] == 255:
             redPixels.append((i,j))
@@ -32,28 +32,16 @@ for i in width:
         else:
             bluePixels.append((i, j))
             histoBlue.append(contrast)
-            histoBlueI.append((100*contrast)/(bird_BW[j, i]+1))
+            histoBlueI.append((10*contrast)/(bird_BW[j, i]+1))
 
 
-d = {'blue': ro.IntVector(histoBlueI), 'bin':ro.IntVector(range(256))}
-dataf = ro.DataFrame(d)
-gpb = ggplot2.ggplot(dataf)
-hpb = gpb + \
-    ggplot2.aes_string(y = 'blue', x='bin') + \
-    ggplot2.geom_histogram()
-hpb.plot()
+graphics = importr('graphics')
+graphics.hist(ro.IntVector(tuple(histoBlue)), main='Histogram of', xlab='Contrast')
 
 grdevices = importr('grDevices')
 grdevices.dev_new()
 
-d = {'red': ro.IntVector(histoRedI)}
-dataf = ro.DataFrame(d)
-print dataf
-gpr = ggplot2.ggplot(dataf)
-hpr = gpr + \
-    ggplot2.aes_string(x = 'red') + \
-    ggplot2.stat_bin()
-hpr.plot()
+graphics.hist(ro.IntVector(tuple(histoRed)), main='Histogram of', xlab='Contrast')
 
 
 cvShowImage("Bird", bird_BW)
